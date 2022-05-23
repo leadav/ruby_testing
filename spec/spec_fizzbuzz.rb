3a097af2e0253628d0cac08070fdbcefe9e0e3d6
Marine Loriot <mloriot@octo.com>
2022-05-23 14:23:33 +0200

    third test

--- spec/spec_fizzbuzz.rb
+++ spec/spec_fizzbuzz.rb
@@ -1,14 +1,14 @@
 require './lib/fizzbuzz'
 
 describe FizzBuzz do
-  it "returns count until 5" do
-    fizzbuzz = FizzBuzz.new
-    expect(fizzbuzz.count()).to eql("1 2 3 4 5")
-  end
-
   it "returns fizz for 3" do
     fizzbuzz = FizzBuzz.new
     expect(fizzbuzz.count()).to eql("1 2 Fizz 4 5")
+  end
+
+  it "returns buzz for 5" do
+    fizzbuzz = FizzBuzz.new
+    expect(fizzbuzz.count()).to eql("1 2 Fizz 4 Buzz")
   end
 end
 
